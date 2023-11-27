9d542422576735442e074d7f8f900ab4d30ea1b6
Hsuan1128 <dewitt1128@gmail.com>
2023-11-27 14:58:07 +0800

    初始化

---
+++ navbar.js
@@ -0,0 +1,23 @@
+// let closeBtn = document.querySelector(".cross");
+
+// let list = document.querySelector(".product-info");
+
+// let openSwitch = document.querySelector(".nav-img");
+// openSwitch.addEventListener("click", () => {
+//   if (list.style.display == "none") {
+//     list.style.display = "block";
+//   } else {
+//     list.style.display = "none";
+//   }
+// });
+
+// closeBtn.addEventListener("click", () => {
+//   list.style.display = "none";
+// });
+
+//聯絡我們js
+let contactBtn = document.querySelector("#contact-us-btn");
+console.log(contactBtn);
+contactBtn.addEventListener("click", () => {
+  alert("感謝您耐心地填寫，後續將盡快於您聯繫!");
+});
