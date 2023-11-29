28af9270e030022a27575cad976303571c616fda
Hsuan1128 <dewitt1128@gmail.com>
2023-11-29 22:03:52 +0800

    1129

--- navbar.js
+++ navbar.js
@@ -23,28 +23,28 @@
 // });
 
 //填寫加盟表單
-let fillIn = document.querySelector("#send");
-console.log(fillIn);
-let form = document.querySelector("#join-form");
-console.log(form);
-let cross = document.querySelector("#cross");
-fillIn.addEventListener("click", () => {
-  if (form.style.display == "none") {
-    form.style.display = "block";
-  } else {
-    form.style.display = "none";
-  }
-});
-cross.addEventListener("click", () => {
-  if (form.style.display == "block") {
-    form.style.display = "none";
-  } else {
-    form.style.display = "block";
-  }
-});
+// let fillIn = document.querySelector("#send");
+// console.log(fillIn);
+// let form = document.querySelector("#join-form");
+// console.log(form);
+// let cross = document.querySelector("#cross");
+// fillIn.addEventListener("click", () => {
+//   if (form.style.display == "none") {
+//     form.style.display = "block";
+//   } else {
+//     form.style.display = "none";
+//   }
+// });
+// cross.addEventListener("click", () => {
+//   if (form.style.display == "block") {
+//     form.style.display = "none";
+//   } else {
+//     form.style.display = "block";
+//   }
+// });
 
-let submit = document.querySelector("#submit");
-submit.addEventListener("click", () => {
-  form.style.display = "none";
-  alert("填寫成功");
-});
+// let submit = document.querySelector("#submit");
+// submit.addEventListener("click", () => {
+//   form.style.display = "none";
+//   alert("填寫成功");
+// });
