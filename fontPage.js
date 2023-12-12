3e86de60b402b1ad318bfa9d3a51ff4608d961e9
Hsuan1128 <dewitt1128@gmail.com>
2023-12-12 21:17:28 +0800

    1212

--- fontPage.js
+++ fontPage.js
@@ -1,9 +1,12 @@
+//banner slide功能
 $(function () {
   let divWidth = $("#slider").width();
+  //div寬度
   let imgCount = $("#slider-content li").length;
-
+  //圖片的總長度
   for (let i = 0; i < imgCount; i++) {
     $("#contentButton").append(`<li></li>`);
+    //有幾張圖片就要加幾個小圓點
   }
   $("#contentButton li:first").addClass("clicked");
 
@@ -41,4 +44,30 @@ $(function () {
     $(`#contentButton li:eq(${index})`).addClass("clicked");
     $("#contentButton li").not(`:eq(${index})`).removeClass("clicked");
   }
+});
+// $('.form_ele').attr('disabled', false);
+//推薦slider
+$(function () {
+  let saleCard = $(`.card-slide`);
+  let curIndex = 0;
+  $(`#left`).click(function () {
+    curIndex--;
+    saleCard.css({ left: -curIndex * 210 });
+    if (curIndex === 0) {
+      $(`#left`).attr("disabled", true);
+    } else {
+      $(`#right`).attr("disabled", false);
+    }
+  });
+
+  $(`#right`).click(function () {
+    curIndex++;
+    saleCard.css({ left: -curIndex * 210 });
+    $(`#left`).attr("disabled", false);
+    if (curIndex === 3) {
+      $(`#right`).attr("disabled", true);
+    } else {
+      $(`#left`).attr("disabled", false);
+    }
+  });
 });
