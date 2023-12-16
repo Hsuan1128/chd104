ec700356e1a4e4ed07bc507fd6e5932c314a2bb0
Hsuan1128 <dewitt1128@gmail.com>
2023-12-16 16:48:54 +0800

    1216

--- login.js
+++ login.js
@@ -1,31 +1,22 @@
-// $(function () {
-//   let acc = 123;
-//   let psw = 123;
+$(function () {
+  let acc = 123;
+  let psw = 123;
 
-//   $(`#submit`).click(() => {
-//     let account = $(`#account`).val();
-//     let password = $(`#password`).val();
-//     if (account == acc && password == psw) {
-//       $(`.id-icon`).css({ display: "block" });
-//     } else {
-//       alert("error");
-//     }
-//   });
-// });
-
-let acc = 123;
-let psw = 123;
-let sub = document.querySelector("#submit");
-sub.addEventListener("click", (event) => {
-  event.preventDefault(); // 阻止表单的默认行为
-  let account = document.querySelector("#account").value;
-  let password = document.querySelector("#password").value;
-  let idIcon = document.querySelector("#id-icon");
-  console.log(idIcon);
-  if (account == acc && password == psw) {
-    idIcon.style.display = "block";
-    alert("ok");
-  } else {
-    alert("error");
-  }
+  $(`#submit`).click((e) => {
+    e.preventDefault();
+    let account = $(`#account`).val();
+    let password = $(`#password`).val();
+    if (account == acc && password == psw) {
+      alert("登入成功");
+      $(`#id-icon`).css({ display: "block" });
+    } else {
+      alert("密碼錯誤");
+    }
+  });
+  $(`#id-icon`).click(() => {
+    $(`#member-page`).toggle();
+  });
+  $(`#logout`).click(() => {
+    location.reload();
+  });
 });
